183eb80cefd00bb3c325de2b884e3c31e1bc6e89
hamasfaa <hamasfa@gmail.com>
2024-12-19 00:45:41 +0700

    nambah msg

--- ROS_Python/src/robot_pkg/src/publisher.py
+++ ROS_Python/src/robot_pkg/src/publisher.py
@@ -2,21 +2,22 @@
 # license removed for brevity
 import rospy
 from robot_pkg.msg import data_robot
+from robot_pkg.msg import bs2pc_roslib
 
 def talker():
-    robotPublisher = rospy.Publisher('data_robot', data_robot, queue_size=10)
+    robotPublisher = rospy.Publisher('/bs2pc_server', bs2pc_roslib, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        data_robot1 = data_robot()
-        data_robot1.epoch = rospy.Time(rospy.get_time())
-        data_robot1.pose_x = 111
-        data_robot1.pose_y = 111
-        data_robot1.theta = 111
+    #     data_robot1 = data_robot()
+    #     data_robot1.epoch = rospy.Time(rospy.get_time())
+    #     data_robot1.pose_x = 111
+    #     data_robot1.pose_y = 111
+    #     data_robot1.theta = 111
 
-        robotPublisher.publish(data_robot1)
-        rospy.loginfo(data_robot1)
-        robotPublisher.publish(data_robot1)
+    #     robotPublisher.publish(data_robot1)
+    #     rospy.loginfo(data_robot1)
+    #     robotPublisher.publish(data_robot1)
         rate.sleep()
 
 if __name__ == '__main__':
